4ebab1d2f95b768d925602241807c68a5ab8a91c
Sayed Haider <sayedhaider2002@gmail.com>
2025-02-27 09:26:51 +0300

    Done

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const verifyToken = require('./middleware/verify-token');
 const testJWTRouter = require('./controllers/test-jwt');
 const usersRouter = require('./controllers/users');
 const profilesRouter = require('./controllers/profiles');
+const hootsRouter = require('./controllers/hoots.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -21,6 +22,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
+app.use('/hoots', hootsRouter);
 
 // Routes
 app.use('/test-jwt', testJWTRouter); // REMOVE FOR TEST ONLY
